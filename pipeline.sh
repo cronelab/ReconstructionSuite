2a0e7eee89643fa79edb3a09900d0a7be203ea7c
TheBrainChain <c.coogan2201@gmail.com>
2019-11-14 15:52:18 -0500

    Removed scipy/fieldtrip dependencies

--- pipeline.sh
+++ pipeline.sh
@@ -19,57 +19,57 @@
 # python ./scripts/coreg.py $1
 # ./scripts/aseg2srf.sh -s $1
 
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_002.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebral-White-Matter.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_002.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebral-White-Matter.obj
 # ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_003.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebral-Cortex.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_004.srf > $SUBJECTS_DIR/$1/obj/Left-Lateral-Ventricle.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_005.srf > $SUBJECTS_DIR/$1/obj/Left-Inf-Lat-Vent.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_007.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebellum-White-Matter.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_008.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebellum-Cortex.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_010.srf > $SUBJECTS_DIR/$1/obj/Left-Thalamus-Proper.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_011.srf > $SUBJECTS_DIR/$1/obj/Left-Caudate.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_012.srf > $SUBJECTS_DIR/$1/obj/Left-Putamen.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_013.srf > $SUBJECTS_DIR/$1/obj/Left-Pallidum.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_014.srf > $SUBJECTS_DIR/$1/obj/3rd-Ventricle.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_015.srf > $SUBJECTS_DIR/$1/obj/4th-Ventricle.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_016.srf > $SUBJECTS_DIR/$1/obj/Brain-Stem.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_017.srf > $SUBJECTS_DIR/$1/obj/Left-Hippocampus.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_018.srf > $SUBJECTS_DIR/$1/obj/Left-Amygdala.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_024.srf > $SUBJECTS_DIR/$1/obj/CSF.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_026.srf > $SUBJECTS_DIR/$1/obj/Left-Accumbens-area.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_028.srf > $SUBJECTS_DIR/$1/obj/Left-VentralDC.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_030.srf > $SUBJECTS_DIR/$1/obj/Left-vessel.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_031.srf > $SUBJECTS_DIR/$1/obj/Left-choroid-plexus.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_041.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebral-White-Matter.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_004.srf > $SUBJECTS_DIR/$1/obj/Left-Lateral-Ventricle.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_005.srf > $SUBJECTS_DIR/$1/obj/Left-Inf-Lat-Vent.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_007.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebellum-White-Matter.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_008.srf > $SUBJECTS_DIR/$1/obj/Left-Cerebellum-Cortex.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_010.srf > $SUBJECTS_DIR/$1/obj/Left-Thalamus-Proper.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_011.srf > $SUBJECTS_DIR/$1/obj/Left-Caudate.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_012.srf > $SUBJECTS_DIR/$1/obj/Left-Putamen.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_013.srf > $SUBJECTS_DIR/$1/obj/Left-Pallidum.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_014.srf > $SUBJECTS_DIR/$1/obj/3rd-Ventricle.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_015.srf > $SUBJECTS_DIR/$1/obj/4th-Ventricle.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_016.srf > $SUBJECTS_DIR/$1/obj/Brain-Stem.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_017.srf > $SUBJECTS_DIR/$1/obj/Left-Hippocampus.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_018.srf > $SUBJECTS_DIR/$1/obj/Left-Amygdala.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_024.srf > $SUBJECTS_DIR/$1/obj/CSF.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_026.srf > $SUBJECTS_DIR/$1/obj/Left-Accumbens-area.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_028.srf > $SUBJECTS_DIR/$1/obj/Left-VentralDC.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_030.srf > $SUBJECTS_DIR/$1/obj/Left-vessel.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_031.srf > $SUBJECTS_DIR/$1/obj/Left-choroid-plexus.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_041.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebral-White-Matter.obj
 # ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_042.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebral-Cortex.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_043.srf > $SUBJECTS_DIR/$1/obj/Right-Lateral-Ventricle.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_044.srf > $SUBJECTS_DIR/$1/obj/Right-Inf-Lat-Vent.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_046.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebellum-White-Matter.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_047.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebellum-Cortex.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_049.srf > $SUBJECTS_DIR/$1/obj/Right-Thalamus-Proper.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_050.srf > $SUBJECTS_DIR/$1/obj/Right-Caudate.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_051.srf > $SUBJECTS_DIR/$1/obj/Right-Putamen.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_052.srf > $SUBJECTS_DIR/$1/obj/Right-Pallidum.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_053.srf > $SUBJECTS_DIR/$1/obj/Right-Hippocampus.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_054.srf > $SUBJECTS_DIR/$1/obj/Right-Amygdala.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_058.srf > $SUBJECTS_DIR/$1/obj/Right-Accumbens-area.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_060.srf > $SUBJECTS_DIR/$1/obj/Right-VentralDC.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_063.srf > $SUBJECTS_DIR/$1/obj/Right-choroid-plexus.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_077.srf > $SUBJECTS_DIR/$1/obj/WM-hypointensities.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_085.srf > $SUBJECTS_DIR/$1/obj/Optic-Chiasm.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_251.srf > $SUBJECTS_DIR/$1/obj/CC_Posterior.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_252.srf > $SUBJECTS_DIR/$1/obj/CC_Mid_Posterior.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_253.srf > $SUBJECTS_DIR/$1/obj/CC_Central.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_254.srf > $SUBJECTS_DIR/$1/obj/CC_Mid_Anterior.obj
-# ./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_255.srf > $SUBJECTS_DIR/$1/obj/CC_Anterior.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_043.srf > $SUBJECTS_DIR/$1/obj/Right-Lateral-Ventricle.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_044.srf > $SUBJECTS_DIR/$1/obj/Right-Inf-Lat-Vent.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_046.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebellum-White-Matter.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_047.srf > $SUBJECTS_DIR/$1/obj/Right-Cerebellum-Cortex.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_049.srf > $SUBJECTS_DIR/$1/obj/Right-Thalamus-Proper.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_050.srf > $SUBJECTS_DIR/$1/obj/Right-Caudate.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_051.srf > $SUBJECTS_DIR/$1/obj/Right-Putamen.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_052.srf > $SUBJECTS_DIR/$1/obj/Right-Pallidum.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_053.srf > $SUBJECTS_DIR/$1/obj/Right-Hippocampus.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_054.srf > $SUBJECTS_DIR/$1/obj/Right-Amygdala.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_058.srf > $SUBJECTS_DIR/$1/obj/Right-Accumbens-area.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_060.srf > $SUBJECTS_DIR/$1/obj/Right-VentralDC.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_063.srf > $SUBJECTS_DIR/$1/obj/Right-choroid-plexus.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_077.srf > $SUBJECTS_DIR/$1/obj/WM-hypointensities.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_085.srf > $SUBJECTS_DIR/$1/obj/Optic-Chiasm.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_251.srf > $SUBJECTS_DIR/$1/obj/CC_Posterior.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_252.srf > $SUBJECTS_DIR/$1/obj/CC_Mid_Posterior.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_253.srf > $SUBJECTS_DIR/$1/obj/CC_Central.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_254.srf > $SUBJECTS_DIR/$1/obj/CC_Mid_Anterior.obj
+./scripts/srf2obj $SUBJECTS_DIR/$1/ascii/aseg_255.srf > $SUBJECTS_DIR/$1/obj/CC_Anterior.obj
 
-# mris_convert $SUBJECTS_DIR/$1/surf/lh.pial $SUBJECTS_DIR/$1/surf/lh.pial.asc
-# mris_convert $SUBJECTS_DIR/$1/surf/rh.pial $SUBJECTS_DIR/$1/surf/rh.pial.asc
-# mv $SUBJECTS_DIR/$1/surf/lh.pial.asc $SUBJECTS_DIR/$1/surf/lh.pial.srf
-# mv $SUBJECTS_DIR/$1/surf/rh.pial.asc $SUBJECTS_DIR/$1/surf/rh.pial.srf
-# ./scripts/annot2dpv $SUBJECTS_DIR/$1/label/lh.aparc.annot $SUBJECTS_DIR/$1/label/lh.aparc.annot.dpv
-# ./scripts/annot2dpv $SUBJECTS_DIR/$1/label/rh.aparc.annot $SUBJECTS_DIR/$1/label/rh.aparc.annot.dpv
-# ./scripts/splitsrf $SUBJECTS_DIR/$1/surf/lh.pial.srf $SUBJECTS_DIR/$1/label/lh.aparc.annot.dpv $SUBJECTS_DIR/$1/rois/lh.pial_roi
-# ./scripts/splitsrf $SUBJECTS_DIR/$1/surf/rh.pial.srf $SUBJECTS_DIR/$1/label/rh.aparc.annot.dpv $SUBJECTS_DIR/$1/rois/rh.pial_roi
+mris_convert $SUBJECTS_DIR/$1/surf/lh.pial $SUBJECTS_DIR/$1/surf/lh.pial.asc
+mris_convert $SUBJECTS_DIR/$1/surf/rh.pial $SUBJECTS_DIR/$1/surf/rh.pial.asc
+mv $SUBJECTS_DIR/$1/surf/lh.pial.asc $SUBJECTS_DIR/$1/surf/lh.pial.srf
+mv $SUBJECTS_DIR/$1/surf/rh.pial.asc $SUBJECTS_DIR/$1/surf/rh.pial.srf
+./scripts/annot2dpv $SUBJECTS_DIR/$1/label/lh.aparc.annot $SUBJECTS_DIR/$1/label/lh.aparc.annot.dpv
+./scripts/annot2dpv $SUBJECTS_DIR/$1/label/rh.aparc.annot $SUBJECTS_DIR/$1/label/rh.aparc.annot.dpv
+./scripts/splitsrf $SUBJECTS_DIR/$1/surf/lh.pial.srf $SUBJECTS_DIR/$1/label/lh.aparc.annot.dpv $SUBJECTS_DIR/$1/rois/lh.pial_roi
+./scripts/splitsrf $SUBJECTS_DIR/$1/surf/rh.pial.srf $SUBJECTS_DIR/$1/label/rh.aparc.annot.dpv $SUBJECTS_DIR/$1/rois/rh.pial_roi
 ./scripts/srf2obj $SUBJECTS_DIR/$1/rois/rh.pial_roi.0001.srf > $SUBJECTS_DIR/$1/obj/rh.bankssts.obj
 ./scripts/srf2obj $SUBJECTS_DIR/$1/rois/rh.pial_roi.0002.srf > $SUBJECTS_DIR/$1/obj/rh.caudalanteriorcingulate.obj
 ./scripts/srf2obj $SUBJECTS_DIR/$1/rois/rh.pial_roi.0003.srf > $SUBJECTS_DIR/$1/obj/rh.caudalmiddlefrontal.obj
@@ -147,8 +147,5 @@
 # mri_annotation2label --subject $1 --hemi "lh" --surface pial --outdir $SUBJECTS_DIR/$1/label/gyri
 # mri_annotation2label --subject $1 --hemi "rh" --surface pial --outdir $SUBJECTS_DIR/$1/label/gyri
 
-#convert the BIS mgrid to a .mat
-# matlab -nojvm -nodesktop -batch "mgridConverter ${1}"
-
 # #Take all the objs and electrodes and create a fbx from them
-/usr/local/blender/blender --background --python scripts/obj2fbx.py
+/usr/local/blender/blender --background --python scripts/obj2fbx.py /$1
--- scripts/obj2fbx.py
+++ scripts/obj2fbx.py
@@ -1,37 +1,39 @@
-
 import bpy
-import scipy.io
 import os
 import sys
 
 def main(patient=''):
-
-    subjDir = "{dir}{patient}".format(
-        dir=os.environ['SUBJECTS_DIR'], patient=patient)
-
-    electrodes = scipy.io.loadmat("{dir}/electrodes/electrodes.mat".format(dir=subjDir))
 
     bpy.ops.object.empty_add(type='CUBE')
     bpy.context.active_object.name = 'Electrodes'
     bpy.ops.object.empty_add(type='CUBE')
     bpy.context.active_object.name = 'aseg'
 
-    for i in range(0, len(electrodes['electrodes']['position'][0][0])):
+    subjDir = "{dir}{patient}".format(
+        dir=os.environ['SUBJECTS_DIR'], patient=patient)
+        
+    electrodes = open("{dir}/electrodes/electrodes.txt".format(dir=subjDir))
+    elecs = electrodes.readlines()
+    for elec in elecs:
+        electrodeGroup = elec.split('\t')[0]
+        electrodeName = elec.split('\t')[2]
+        electrodeX = float(elec.split('\t')[3])
+        electrodeY = float(elec.split('\t')[4])
+        electrodeZ = float(elec.split('\t')[5])
         bpy.ops.mesh.primitive_ico_sphere_add(
-            location=electrodes['electrodes']['position'][0][0][i])
-        bpy.context.active_object.name = electrodes['electrodes']['label'][0][0][i][0][0]
+            location=(electrodeX, electrodeY, electrodeZ))
+        bpy.context.active_object.name = electrodeName
         bpy.context.active_object.parent = bpy.data.objects['Electrodes']
-
 
     for file in os.listdir("{dir}/obj".format(dir=subjDir)):
         if file.endswith(".obj"):
             bpy.ops.import_scene.obj(
                 filepath="{dir}/obj/".format(dir=subjDir) + file)
+            bpy.context.active_object.rotation_euler = (-90,0,0)
             bpy.data.objects[os.path.splitext(
                 file)[0]].parent = bpy.data.objects['aseg']
 
     bpy.ops.export_scene.fbx(filepath="{dir}/{patient}".format(dir=subjDir,patient=patient) + ".fbx")
-
 
 if __name__ == "__main__":
     main(sys.argv[4])
