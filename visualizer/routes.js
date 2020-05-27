caebe4c3c1f1f3e578d970d674df9f4b231e7424
TheBrainChain <c.coogan2201@gmail.com>
2020-05-27 19:57:01 -0400

    viz subject via env variable

--- visualizer/routes.js
+++ visualizer/routes.js
@@ -1,6 +1,9 @@
 import path from "path";
 import fs from "fs";
 
+let subject =  process.env.SUBJECT
+let dataDir = '/data/derivatives/freesurfer/'
+
 let __dirname = path.resolve(path.dirname(""));
 const routes = (express) => {
   const router = express.Router();
@@ -9,26 +12,25 @@ const routes = (express) => {
     res.sendFile(path.join(__dirname, "/dist", "/index.html"))
   );
 
-  let dataDir = '/data/derivatives/freesurfer/'
 
   //3D brain
   router.get("/api/brain3D", (req, res) => {
     if (
-      fs.existsSync(`${dataDir}/PY20N006/reconstruction.glb`)
+      fs.existsSync(`${dataDir}/${subject}/reconstruction.glb`)
     ) {
       console.log("File exists. 1");
       res.sendFile(`reconstruction.glb`, {
-        root: `${dataDir}/PY20N006/`,
+        root: `${dataDir}/${subject}/`,
       });
     }
   });
 
   router.get("/api/nifti", (req, res) => {
     if (
-      fs.existsSync(`${dataDir}/PY20N006/reconstruction.nii`)
+      fs.existsSync(`${dataDir}/${subject}/reconstruction.nii`)
     ) {
       console.log("File exists. 2");
-      res.sendFile(`${dataDir}/PY20N006/reconstruction.nii`);
+      res.sendFile(`${dataDir}/${subject}/reconstruction.nii`);
     } else {
       console.log("does NOT exist");
       res.status(204).end();
